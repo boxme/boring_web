c814e7aede01d170184b43e235678bb11561949b
desmond <desmond@replaid.com>
2018-09-10 21:29:11 -0700

    Test postgres db

--- exp/main.go
+++ exp/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	post   = 5432
+	user   = "desmond"
+	dbname = "lenslocked_dev"
 )
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", host, post, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
-	data := struct {
-		Name string
-	}{"John Smith"}
-
-	err = t.Execute(os.Stdout, data)
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("Successfully connected!")
+	db.Close()
 }
