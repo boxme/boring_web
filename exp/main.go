f84acbe2963f83756cedcb13864015f1002d7da6
desmond <desmond@replaid.com>
2018-09-11 22:13:14 -0700

    Insert records

--- exp/main.go
+++ exp/main.go
@@ -26,6 +26,25 @@ func main() {
 	}
 
 	var id int
+	for i := 1; i < 6; i++ {
+		userId := 1
+		if i > 3 {
+			userId = 2
+		}
+		amount := 1000 * i
+		description := fmt.Sprintf("USB-C Adapter x%d", i)
+
+		err = db.QueryRow(`
+	    INSERT INTO orders (user_id, amount, description)
+	    VALUES ($1, $2, $3)
+	    RETURNING id`, userId, amount, description).Scan(&id)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Created an order with the ID:", id)
+	}
+
 	var name, email string
 	rows, err := db.Query(`SELECT id, name, email FROM users WHERE email=$1 OR ID > $2`, "dnyy@email.com", 3)
 	if err != nil {
