49554d77bdce8f8ea7e02c88c53afb547fcceb1e
desmond <desmond@replaid.com>
2018-10-02 22:29:05 -0700

    Encapsulate alert with data

--- controllers/users.go
+++ controllers/users.go
@@ -31,11 +31,23 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 		Level   string
 		Message string
 	}
+
+	type Data struct {
+		Alert *Alert
+		Yield interface{}
+	}
+
 	alert := Alert{
 		Level:   "success",
 		Message: "Successfully rendered a dynamic alert!",
 	}
-	if err := u.NewView.Render(w, alert); err != nil {
+
+	data := Data{
+		Alert: &alert,
+		Yield: "this can be any type",
+	}
+
+	if err := u.NewView.Render(w, data); err != nil {
 		panic(err)
 	}
 }
