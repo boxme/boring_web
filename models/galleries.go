2aa2c9e1656752990ba70fdb0d70ffdfb7e9ff23
desmond <desmond@replaid.com>
2018-10-10 21:55:20 -0700

    Validation for gallery creation

--- models/galleries.go
+++ models/galleries.go
@@ -2,6 +2,11 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+const (
+	ErrUserIDRequired modelError = "models: user ID is required"
+	ErrTitleRequired  modelError = "models: title is required"
+)
+
 type Gallery struct {
 	gorm.Model
 	UserID uint   `gorm:"not_null;index"`
@@ -28,6 +33,8 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+type galleryValFn func(*Gallery) error
+
 var _ GalleryDB = &galleryGorm{}
 
 func NewGalleryService(db *gorm.DB) GalleryService {
@@ -39,7 +46,41 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 		},
 	}
 }
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(gallery, gv.userIDRequired, gv.titleRequired)
+	if err != nil {
+		return err
+	}
+
+	return gv.GalleryDB.Create(gallery)
+}
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+
+	return nil
+}
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
