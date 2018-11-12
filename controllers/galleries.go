6b5324a542175af4c6d64510e3cb4465fe6a7ad6
desmond <desmond@replaid.com>
2018-11-11 16:09:16 -0800

    Get all of gallery's images from the gallery ID

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -251,5 +251,9 @@ func (g *Galleries) galleryByID(w http.ResponseWriter, r *http.Request) (*models
 		return nil, err
 	}
 
+	// Get all images of this gallery
+	images, _ := g.is.ByGalleryID(gallery.ID)
+	gallery.Images = images
+
 	return gallery, nil
 }
